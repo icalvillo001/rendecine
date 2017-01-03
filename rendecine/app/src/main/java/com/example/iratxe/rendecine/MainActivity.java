7b18c4c1fa72601832606fb97ce73e27c670896a
icalvillo001 <icalvillo001@ehu.es>
2017-01-03 23:47:28 +0100

    Login y registro

--- rendecine/app/src/main/java/com/example/iratxe/rendecine/MainActivity.java
+++ rendecine/app/src/main/java/com/example/iratxe/rendecine/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.iratxe.rendecine;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void castellano(View v){
+        Intent intent=new Intent(this,RegistroActivity.class);
+        startActivity(intent);
+    }
+    public void euskera(View v){
+        Intent intent=new Intent(this,RegistroActivity.class);
+        startActivity(intent);
     }
 }
